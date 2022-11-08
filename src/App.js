e5b01867bffd2f0469821df4007114f381cbb8e3
Sergey <santerakis@gmail.com>
2022-11-08 17:15:05 +0200

    folders

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Header from './Header.js'
+import Technologies from './Technologies.js'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      компанента = Simple HTML
-    </div>
-  );
+    <div>
+      <Header  /> 
+      <Technologies /> 
+    </div>);
 }
 
 export default App;
